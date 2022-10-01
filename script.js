db4001f2009514f0602102f355980af2bf69d297
alona5 <alenaprager@hotmail.com>
2022-10-02 02:10:23 +0300

    Added Modal popup

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('Hello!');
+const modal = document.getElementById('modal')
+const closeModal = document.getElementById('modal-close-btn')
+
+setTimeout(function(){
+  modal.style.display = 'inline'
+}, 1500)
+
+closeModal.addEventListener('click', function(){
+  modal.style.display = 'none'
+})
