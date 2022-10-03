250f513907193c1c24102dea6b2d302330b411fb
alona5 <alenaprager@hotmail.com>
2022-10-03 19:48:56 +0300

    Added messages to the Modal

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const modal = document.getElementById('modal')
 const closeModal = document.getElementById('modal-close-btn')
+const consentForm = document.getElementById('consent-form')
+const modalText = document.getElementById('modal-text')
 
 setTimeout(function(){
   modal.style.display = 'inline'
@@ -8,3 +10,27 @@ setTimeout(function(){
 closeModal.addEventListener('click', function(){
   modal.style.display = 'none'
 })
+
+consentForm.addEventListener('submit', function(e){
+  e.preventDefault()
+  modalText.innerHTML = `
+    <div class="modal-inner-loading">
+      <img src="images/loading.svg" class="loading">
+      <p id="uploadText">
+      Uploading your data to the dark web...
+      </p>
+    </div>`
+
+    setTimeout(function() {
+      document.getElementById('uploadText').innerText = `Making the sale...`
+    }, 1500)
+
+    setTimeout(function(){
+      document.getElementById('modal-inner').innerHTML = `
+      <h2>Thanks you sucker! </h2>
+      <p>We just sold the rights to your eternal soul.</p>
+      <div class="idiot-gif">
+        <img src="images/pirate.gif">
+      </div>`
+    }, 3000)
+}) 
